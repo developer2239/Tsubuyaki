47e5e6d92a13d7a9398820ff036f1577c5a98087
0901JP <mb6a3b@bma.biglobe.ne.jp>
2024-02-05 10:46:03 +0900

    #18 auto_increment追記

--- SQL/initialize.sql
+++ SQL/initialize.sql
@@ -1,42 +1,25 @@
 
--- DB作成
-
 DROP DATABASE IF EXISTS tsubuyaki_db;
 CREATE DATABASE tsubuyaki_db;
 
 USE tsubuyaki_db;
 
---------------------------------------------------------------------------------
--- account のレイアウト変更
---   注意！！：テーブルに依存するオブジェクト（ビューなど）が削除される場合があります。それらのオブジェクトは復元されません。
---   2024/02/01 0901JP
---------------------------------------------------------------------------------
 
--- accountテーブルの作成
 create table account (
-  account_id INT not null
+  account_id INT NOT NULL AUTO_INCREMENT
   , id VARCHAR(10) not null
   , name VARCHAR(10) not null
-  , password VARCHAR(255) not null
+  , password VARCHAR(256) not null
   , created_at DATETIME default CURRENT_TIMESTAMP not null
   , updated_at DATETIME default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null
   , primary key (account_id,id)
 );
 
-
---------------------------------------------------------------------------------
--- post のレイアウト変更
---   注意！！：テーブルに依存するオブジェクト（ビューなど）が削除される場合があります。それらのオブジェクトは復元されません。
---   2024/02/01 0901JP
---------------------------------------------------------------------------------
-
--- postテーブルの作成
 create table post (
-  post_id INT not null
+  post_id INT NOT NULL AUTO_INCREMENT
   , account_id INT not null
   , content VARCHAR(50) not null
   , created_at DATETIME default CURRENT_TIMESTAMP not null
   , updated_at DATETIME default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null
   , primary key (post_id)
 );
-/
