75b3a4b5c0e6f5e1383ef72dfbe7a97deda65586
0901JP <mb6a3b@bma.biglobe.ne.jp>
2024-02-05 10:46:29 +0900

    #18 アカウント新規登録の際のPOSTロジック修正

--- tsubuyaki/tsubuyaki/views.py
+++ tsubuyaki/tsubuyaki/views.py
@@ -1,5 +1,7 @@
+from hashlib import sha256
 from django.shortcuts import render
 from django.shortcuts import redirect
+from tsubuyaki.models.account import Account
 
 # ログイン画面表示
 def login(request):
@@ -10,7 +12,18 @@ def login(request):
 
 # 新規アカウント作成画面
 def signup(request):
-    return render(request,"signup.html")
+    if request.method == "GET":
+        return render(request,"signup.html")
+    elif request.method == "POST":
+        # パラメータ取得
+        name = request.POST.get("name")
+        id = request.POST.get("id")
+        password = request.POST.get("password")
+        # パスワードのハッシュ化
+        hashed_password = sha256(password.encode()).hexdigest()
+        Account.objects.create(name = name, id = id, password = hashed_password)
+        return redirect("/login")
+
 
 # つぶやき・ポスト一覧画面
 def posts(request):
