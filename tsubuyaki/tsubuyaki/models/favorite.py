fe553b6716ed758741199c7f4e27629285b2e30a
0901JP <mb6a3b@bma.biglobe.ne.jp>
2024-02-08 15:45:46 +0900

    #38 エンティティ修正

--- tsubuyaki/tsubuyaki/models/favorite.py
+++ tsubuyaki/tsubuyaki/models/favorite.py
@@ -5,7 +5,8 @@ from tsubuyaki.models.post import Post
 # favorite エンティティ
 class Favorite(models.Model):
 
-    post = models.ForeignKey(Post,on_delete = models.CASCADE,primary_key = True)
+    favorite_id = models.IntegerField(primary_key = True)
+    post = models.ForeignKey(Post,on_delete = models.CASCADE)
     account = models.ForeignKey(Account,on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add = True)
 
